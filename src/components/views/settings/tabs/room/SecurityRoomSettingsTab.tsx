ed0cc404f9bedc95430d52e3b5f7e1e1111cbe1a
Dileep Bandla <dileepb@rapyd.net>
2026-01-27 14:42:51 +0400

    Remove learn more URL

--- src/components/views/settings/tabs/room/SecurityRoomSettingsTab.tsx
+++ src/components/views/settings/tabs/room/SecurityRoomSettingsTab.tsx
@@ -458,12 +458,12 @@ export default class SecurityRoomSettingsTab extends React.Component<IProps, ISt
             <>
                 {_t(
                     "room_settings|security|history_visibility_warning",
-                    {},
-                    {
-                        a: (sub) => (
-                            <ExternalLink href="https://element.io/en/help#e2ee-history-sharing">{sub}</ExternalLink>
-                        ),
-                    },
+                    // {},
+                    // {
+                    //     a: (sub) => (
+                    //         <ExternalLink href="https://element.io/en/help#e2ee-history-sharing">{sub}</ExternalLink>
+                    //     ),
+                    // },
                 )}
             </>
         );
